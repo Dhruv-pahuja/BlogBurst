2bd801add7cd1540f6926791fbd63320c86f311d
Dhruv Pahuja <dhruvpahuja013@gmail.com>
2024-07-02 12:55:37 +0530

    debug

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Editor } from '@tinymce/tinymce-react';
 import {Controller } from 'react-hook-form';
-
+import conf from '../conf/conf';
 
 export default function RTE({name, control, label, defaultValue =""}) {
   return (
@@ -13,7 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-        apiKey='bxfquvqht98bqqioq3mzwrrkggjgemnhlyl8rtlvj834779u'
+        apiKey={conf.tinymceApiKey}
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
