b041fb33240bc06d4cdb6b07a2219ae65f09c350
Dhruv Pahuja <dhruvpahuja013@gmail.com>
2024-07-02 13:33:58 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,12 @@ dotenv.config();
 export default defineConfig({
   plugins: [react()],
   define: {
-    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    tinymceApiKey: String(import.meta.env.VITE_TINYMCE_API_KEY),
+    appwriteUrl: JSON.stringify(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: JSON.stringify(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: JSON.stringify(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: JSON.stringify(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: JSON.stringify(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    tinymceApiKey: JSON.stringify(import.meta.env.VITE_TINYMCE_API_KEY),
   },
   base: '/BlogBurst/', // Replace 'your-repo-name' with your actual repository name
   build: {
