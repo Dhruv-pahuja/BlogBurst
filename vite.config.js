04228aade338e238455d3580787c35b4626bdf8a
Dhruv Pahuja <dhruvpahuja013@gmail.com>
2024-07-02 13:31:37 +0530

    added github deployement config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    tinymceApiKey: String(import.meta.env.VITE_TINYMCE_API_KEY),
+  },
+  base: '/BlogBurst/', // Replace 'your-repo-name' with your actual repository name
+  build: {
+    outDir: 'docs', // Change 'docs' to any other directory name if needed
+  },
 })
