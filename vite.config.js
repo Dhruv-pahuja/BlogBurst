2ac919da7cbc209b8d367c9e79107bc15fabde3a
Dhruv Pahuja <dhruvpahuja013@gmail.com>
2024-07-02 13:36:01 +0530

    2 update vite config

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,7 @@ dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    appwriteUrl: JSON.stringify(import.meta.env.VITE_APPWRITE_URL),
-    appwriteProjectId: JSON.stringify(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwriteDatabaseId: JSON.stringify(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwriteCollectionId: JSON.stringify(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwriteBucketId: JSON.stringify(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    tinymceApiKey: JSON.stringify(import.meta.env.VITE_TINYMCE_API_KEY),
-  },
+  
   base: '/BlogBurst/', // Replace 'your-repo-name' with your actual repository name
   build: {
     outDir: 'docs', // Change 'docs' to any other directory name if needed
